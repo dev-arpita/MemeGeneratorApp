2281690df08efd04f8320524b6f99a4560a07046
Arpita Roy <arpita.roy.workar@gmail.com>
2023-04-08 20:00:07 +0530

    Meme Generator: Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="logo-section">
+                <img className="logo-img" src='/images/troll-face.png' />
+                <h1 className="header-title">Meme Generator</h1>
+            </div>
+            <p className="header-text">React Course - Project 3</p>
+        </header>
+    )
+}
+export default Header
