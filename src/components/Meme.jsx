c7fb2513ac095b35ff6ce0f3ca37825e894f851a
Arpita Roy <arpita.roy.workar@gmail.com>
2023-04-13 11:41:20 +0530

    Props vs. State Quiz!

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,22 +2,16 @@ import React from "react"
 import memesData from "../memesData.js"
 
 export default function Meme() {
-    /**
-     * Challenge: Get a random image from the `memesData` array
-     * when the "new meme image" button is clicked.
-     *
-     * Log the URL of the image to the console. (Don't worry
-     * about displaying the image yet)
-     */
     let url
-
-    function getMemeImage() {
         const memesArray = memesData.data.memes
+    function getMemeImage() {
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         url = memesArray[randomNumber].url
         console.log(url)
     }
+const renderMemeImg = memesArray.map( img => <img src={img.url}/>)
 
+ console.log(renderMemeImg)
     return (
         <main>
             <p>{url}</p>
@@ -34,10 +28,10 @@ export default function Meme() {
                 />
                 <button
                     className="form--button"
-                    onClick={getMemeImage}
-                >
+                    onClick={getMemeImage}>
                     Get a new meme image 🖼
                 </button>
+                {renderMemeImg}
             </div>
         </main>
     )
